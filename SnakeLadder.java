dd9958f301a19a1346f9c08d1b6d23f1c3feae82
Rahu206 <rahuldeshpande93@gmail.com>
2021-07-06 00:04:09 +0530

    uc3

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,8 +1,31 @@
 public class SnakeLadder{
-	public static void main(String args[]){
+public static void main(String args[]){
 		System.out.println("Welcome to the player");
-		int postion = 0;
-      int score = (int) Math.floor(Math.random()*10)%6;
-		System.out.println(score);
-    }
-  }
+		int position = 0;
+		int roll = 0;
+		int score;
+		//System.out.println(score);
+		int run =(int) Math.floor(Math.random()*10)%3;
+		switch(run){
+		case 1:
+                	score = (int) Math.floor(Math.random()*10)%6;
+			position += score;
+			roll++;
+			break;
+		case 2:
+                	score = (int) Math.floor(Math.random()*10)%6;
+			if (position >= score){
+			position -= score;
+			}else {
+			position = 0;
+			}
+			roll++;
+			break;
+		default:
+			System.out.println("No chance to play");
+			roll++;
+		}
+		System.out.println(position);
+                System.out.println(roll);
+	}
+}
