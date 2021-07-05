a3ef8f2fe142838ef72948afead7efc967146e14
Rahu206 <rahuldeshpande93@gmail.com>
2021-07-06 00:00:14 +0530

    uc2

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,5 +2,7 @@ public class SnakeLadder{
 	public static void main(String args[]){
 		System.out.println("Welcome to the player");
 		int postion = 0;
-}
-}
+      int score = (int) Math.floor(Math.random()*10)%6;
+		System.out.println(score);
+    }
+  }
